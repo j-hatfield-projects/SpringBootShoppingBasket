fcf15021b7253471aac7544d204f6e0b8155d09c
jon-hatfield-projects <jondevtests@gmail.com>
2019-02-10 20:19:05 +0000

    Add Swagger docs and UI

--- src/main/java/com/shop/controller/CheckOutController.java
+++ src/main/java/com/shop/controller/CheckOutController.java
@@ -1,6 +1,8 @@
 package com.shop.controller;
 
 import com.shop.service.CheckOutService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping("/checkout")
+@Api(description = "API for managing a shopping basket")
 public class CheckOutController {
 
     CheckOutService checkOutService;
@@ -22,24 +25,28 @@ public class CheckOutController {
 
     @RequestMapping(value = "/order/{pricingRulesId}", method = POST)
     @ResponseBody
+    @ApiOperation("Creates an order")
     public Long createOrder(@PathVariable("pricingRulesId") Long pricingRulesId) {
         return checkOutService.createOrder(pricingRulesId);
     }
 
     @RequestMapping(value = "/order/{orderId}/item/{itemId}", method = POST)
     @ResponseBody
+    @ApiOperation("Scans (adds) an item to an order")
     public void scanItem(@PathVariable("orderId") Long orderId, @PathVariable("itemId") Long itemId) {
         checkOutService.scanItem(orderId, itemId);
     }
 
     @RequestMapping(value = "/order/{orderId}/item/{itemId}", method = DELETE)
     @ResponseBody
+    @ApiOperation("Voids (removes) an item from an order")
     public void voidItem(@PathVariable("orderId") Long orderId, @PathVariable("itemId") Long itemId) {
         checkOutService.voidItem(orderId, itemId);
     }
 
     @RequestMapping(value = "/order/{orderId}", method = GET)
     @ResponseBody
+    @ApiOperation("Gets an order summary")
     public String getOrder(@PathVariable("orderId") Long orderId) {
         return checkOutService.getOrder(orderId);
     }
